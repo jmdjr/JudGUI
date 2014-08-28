d5275416ff587c69a0b535704667f4bad26f973e
jmdjr <jmdcodejunkie@gmail.com>
2014-08-28 16:19:00 -0700

    updated frames to have backgrounds
    adding in new functionality to enable quick adding of background
    images/shapes to Frames, Frame Collections and the Engine as a whole.
    not completed, but will allow for default content to be made.  button
    work used for backgrounds.  started new Frame for Buttons.

--- JudGUI/JudGUI/Scripts/judGUI/Frames/Frame.js
+++ JudGUI/JudGUI/Scripts/judGUI/Frames/Frame.js
@@ -3,40 +3,54 @@
         Frame Constructor
 *********************************************************************************************/
 
-define(['jquery', 'Util/Utils', 'Frames/Transitions'], function ($) {
+define(['jquery', 'Util/Utils', 'Frames/Transitions', 'Frames/Backgrounds'], function ($) {
     (function (scope) {
         var judgui = scope.judgui || {};
 
-        var Frame = function (initializer) {
-            this.initialize(initializer);
+
+        var Frame = function (design, initializer) {
+            this.initialize(design, initializer);
         }
 
         var p = Frame.prototype = new createjs.Container();
         Frame.prototype.inherited_init = p.initialize;
 
-        p.Engine = null;
         p.bounds = null;
 
-        p.initialize = function (initializer) {
+        p.initialize = function (design, initializer) {
             if (this.inherited_init) this.inherited_init();
 
             judgui.MakeTransitionable.call(this);
+            judgui.MakeFramingBackground.call(this);
+
             this.bounds = this.getBounds();
 
-            if (typeof initializer === 'function') {
-                initializer.call(this);
+            if (typeof design === 'object') {
+
+                this._extendStyle(design);
+
+                if (typeof initializer === 'function') {
+                    initializer.call(this);
+                }
+
+            } else if (typeof design === 'function') {
+                design.call(this);
             }
 
             return this;
         }
 
         // adds a new element to this frame based on name, accepts multiple arguments for initializing the element
+        // returns whether or not adding the element was attempted.
         p.add = function (name, initialize) {
             if (name instanceof createjs.DisplayObject) {
                 this.addChild(name);
-            } else if (!(!(judgui[name]) || !(judgui[name]._Spawner))) {
+                return true;
+            } else if (!judgui.IsUndefined(judgui[name]) && !judgui.IsUndefined(judgui[name]._Spawner)) {
                 this.addChild(new judgui[name]._Spawner(initialize));
+                return true;
             }
+            return false;
         }
 
         judgui.Frame = Frame;
